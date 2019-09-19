11aefa4b0b77c61ad445d1199d309e11d476a8d3
Marshall Warners <MWALQM@hermanmiller.com>
2019-09-18 22:06:31 -0400

    testing

--- bot.js
+++ bot.js
@@ -15,6 +15,7 @@ var announceID = '619358591740018698';
 var consoleID = '622937431586373633';
 var testingID = '621375349334343690';
 var backupFlag = 0;
+//this is a test
 
 (function(){
     setInterval(async function() {
